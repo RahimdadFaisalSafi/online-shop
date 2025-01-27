9275b11edc41248f9728108c15425a574f7bcb4d
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2025-01-27 17:24:04 +0100

    Add navigation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <div className='bg-amber-500'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-semibold">SneakerStore</h1>
+        <nav>
+          <ul className="flex space-x-6">
+            <li>
+              <a href="#" className="hover:text-gray-300">Home</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">Products</a>
+            </li>
+            <li>
+              <a href="3" className="hover:text-gray-300">Cart</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
